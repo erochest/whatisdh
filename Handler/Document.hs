264ac7ee11a26688636d863f5799abaf4984c763
Eric Rochester <err8n@virginia.edu>
2012-06-21 05:36:54 -0400

    GET DocNewR Handler.

--- Handler/Document.hs
+++ Handler/Document.hs
@@ -12,6 +12,7 @@ module Handler.Document
     , docForm
     ) where
 
+import           Data.Time
 import           Data.Monoid
 import qualified Data.Text as T
 import           Import
@@ -30,7 +31,11 @@ getDocListR = do
 -- DocNewR
 
 getDocNewR :: Handler RepHtml
-getDocNewR = undefined
+getDocNewR = do
+    let action = DocNewR
+        mdoc   = Nothing
+    ((result, form), enctype) <- runFormPost $ docForm Nothing
+    defaultLayout $(widgetFile "docedit")
 
 postDocNewR :: Handler RepHtml
 postDocNewR = undefined
@@ -58,13 +63,24 @@ postDocDeleteR docId = undefined
 
 -- Forms
 
+data DocumentInfo = DocumentInfo
+    { diTitle   :: T.Text
+    , diSource  :: Maybe T.Text
+    , diContent :: Maybe T.Text
+    , diFile    :: Maybe FileInfo
+    }
+
 docAForm :: (Yesod m, RenderMessage m FormMessage)
-         => Maybe Document -> AForm s m Document
-docAForm mdoc = undefined
+         => Maybe Document -> AForm s m DocumentInfo
+docAForm mdoc =   DocumentInfo
+              <$> areq textField "Title"   (documentTitle   <$> mdoc)
+              <*> aopt textField "Source"  (documentSource  <$> mdoc)
+              <*> aopt textField "Content" (Just . documentContent <$> mdoc)
+              <*> fileAFormOpt "Upload File"
 
 docForm :: (Yesod m, RenderMessage m FormMessage)
         => Maybe Document
         -> Markup
-        -> MForm s m (FormResult Document, GWidget s m ())
+        -> MForm s m (FormResult DocumentInfo, GWidget s m ())
 docForm = renderBootstrap . docAForm
 
