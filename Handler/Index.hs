4bda3629cd8822e6cd7fb0cda14f38bd9dc2d6f8
Eric Rochester <err8n@virginia.edu>
2012-06-28 23:03:52 -0400

    Added scripts and style to the index page.

--- Handler/Index.hs
+++ Handler/Index.hs
@@ -5,12 +5,21 @@ module Handler.Index
     , postReindexR
     ) where
 
+
 import           Import
+import           Text.Coffee
+
 
 getIndexR :: Handler RepHtml
 getIndexR = do
     defaultLayout $ do
         setTitle "What is DH? Token Index"
+
+        addScript $ StaticR js_jquery_1_7_2_min_js
+        addScript $ StaticR js_underscore_min_js
+        addScript $ StaticR js_backbone_min_js
+
+        toWidget $(coffeeFile "templates/index.coffee")
         $(widgetFile "index")
 
 getReindexR :: Handler RepHtml
