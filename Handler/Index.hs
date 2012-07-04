628508e9ab25d916eb203de9e052913821e1419d
Eric Rochester <err8n@virginia.edu>
2012-07-03 22:57:21 -0400

    Start on /index page with AJAX.

--- Handler/Index.hs
+++ Handler/Index.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Handler.Index
     ( getIndexR
@@ -6,21 +7,57 @@ module Handler.Index
     ) where
 
 
+import           Control.Applicative
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import           Data.Maybe (catMaybes)
+import           Data.Monoid
+import qualified Data.Text as T
+import           Database.Persist
+import           Database.Persist.GenericSql
+import           Database.Persist.GenericSql.Raw
 import           Import
 import           Text.Coffee
 
 
-getIndexR :: Handler RepHtml
+getIndexR :: Handler RepHtmlJson
 getIndexR = do
-    defaultLayout $ do
-        setTitle "What is DH? Token Index"
+    let select = " SELECT tc.id, b.id, t1.text, t2.text, t3.text \
+                   FROM token_chain tc \
+                   JOIN bigram b ON tc.bigram=b.id \
+                   JOIN token_type t1 ON b.fst_token=t1.id \
+                   JOIN token_type t2 ON b.snd_token=t2.id \
+                   JOIN token_type t3 ON tc.next=t3.id \
+                   "
+    pagination <- runInputGet paginationForm
+    results <- case pagination of
+        (Pagination moffset mlimit morderby msort) -> do
+            let sql = T.concat $ [select] ++ catMaybes
+                        [ (mappend " ORDER BY ")               <$> morderby
+                        , (mappend " ")                        <$> msort
+                        , (mappend " LIMIT " . T.pack . show)  <$> mlimit
+                        , (mappend " OFFSET " . T.pack . show) <$> moffset
+                        , Just ";"
+                        ]
+            $(logDebug) ("SQL: " `mappend` sql)
+            results <- runDB . C.runResourceT $ withStmt sql [] C.$$ CL.consume
+            undefined
+        _ -> return []
 
-        addScript $ StaticR js_jquery_1_7_2_min_js
-        addScript $ StaticR js_underscore_min_js
-        addScript $ StaticR js_backbone_min_js
+    let html = do
+            setTitle "What is DH? Token Index"
 
-        toWidget $(coffeeFile "templates/index.coffee")
-        $(widgetFile "index")
+            addScript $ StaticR js_jquery_1_7_2_min_js
+            addScript $ StaticR js_underscore_min_js
+            addScript $ StaticR js_backbone_min_js
+            addScript $ StaticR js_backbone_paginator_min_js
+
+            toWidget $(coffeeFile "templates/index.coffee")
+            $(widgetFile "index")
+
+        json = ()
+
+    defaultLayoutJson html json
 
 getReindexR :: Handler RepHtml
 getReindexR = undefined
--- templates/index.coffee
+++ templates/index.coffee
@@ -1,3 +1,18 @@
 
 $ ->
+  TokenChain  = Backbone.Model.extend()
+  TokenChains = Backbone.Paginator.requestPager.extend
+    model : TokenChain
+    url   : '@{IndexR}'
+
+    skipAttribute    : '$offset'
+    perPageAttribute : '$limit'
+    sortAttribute    : '$sort'
+    orderAttribute   : '$orderBy'
+
+    perPage : 100
+
+    parse : (response) ->
+      @totalPages = Math.floor(response.count / @perPage)
+      response.results
 
