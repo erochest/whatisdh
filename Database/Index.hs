5acbd58edd2745fc2f0fa129f0529a7b3d49b455
Eric Rochester <err8n@virginia.edu>
2012-07-18 20:38:23 +0200

    Remove unused database helpers and re-implement deleteing the index.

--- Database/Index.hs
+++ Database/Index.hs
@@ -22,33 +22,7 @@ import           Database.Persist
 import           Database.Persist.GenericSql.Raw
 import           Database.Persist.Store
 import           Import
-import           Text.Tokenizer (tokenize)
 import           System.IO (stdout, hFlush)
-
-withTmpObj :: MonadIO m => T.Text -> T.Text -> T.Text -> SqlPersist m a -> SqlPersist m a
-withTmpObj objType name createSql action = do
-    execSql createSql
-    a <- action
-    execSql $ mconcat [ "DROP ", objType, " ", name, ";" ]
-    return a
-
-withTmpTable :: MonadIO m => T.Text -> T.Text -> SqlPersist m a -> SqlPersist m a
-withTmpTable = withTmpObj "TABLE"
-
-withTmpView :: MonadIO m => T.Text -> T.Text -> SqlPersist m a -> SqlPersist m a
-withTmpView = withTmpObj "VIEW"
-
-execSeq :: MonadIO m => T.Text -> [a] -> (a -> [PersistValue]) -> SqlPersist m ()
-execSeq sql params coerce = mapM_ (execute sql) $ map coerce params
-
-execSql :: MonadIO m => T.Text -> SqlPersist m ()
-execSql sql = execute sql []
-
-upInsertUp :: MonadIO m => T.Text -> T.Text -> SqlPersist m ()
-upInsertUp updateSql insertSql = mapM_ execSql [ updateSql
-                                               , insertSql
-                                               , updateSql
-                                               ]
 
 -- This indexes the documents into the TokenType and TokenIndex tables.
 indexDocs :: MonadIO m => [Entity Document] -> SqlPersist m ()
@@ -59,7 +33,6 @@ indexDocs docs = do
     -- Bigram
     -- TokenChain
 
-
 logLine :: MonadIO m => String -> m ()
 logLine msg = liftIO (putStrLn msg >> hFlush stdout)
 
@@ -67,8 +40,7 @@ type IndexKey = (Int, T.Text)
 
 deleteIndex :: forall (b :: (* -> *) -> * -> *) (m :: * -> *). (MonadIO (b m), PersistQuery b m)
             => b m ()
-deleteIndex =
-    undefined
+deleteIndex = updateWhere [] [DocumentTrigrams =. Nothing]
 
 reIndexAll :: forall (m :: * -> *)
            .  (MonadUnsafeIO m, MonadThrow m, MonadIO m, MonadBaseControl IO m)
